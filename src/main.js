c75725fed29bdc326f87e89e89df80337284ec8a
muhamdfikrii <fikri5741@gmail.com>
2021-02-17 18:42:38 +0700

    Fixing z index on header

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@
 
 const hamburger = document.querySelector(".hamburger");
 hamburger.addEventListener("click", function () {
-  const hamburgerWrapper = document.querySelector(".hamburger-wrapper");
-  hamburgerWrapper.classList.toggle("hidden");
+  // const hamburgerWrapper = document.querySelector(".hamburger-wrapper");
+  // hamburgerWrapper.classList.toggle("hidden");
 
   const hamburgerMenu = document.querySelector(".hamburger-wrapper ul");
+  hamburgerMenu.classList.toggle("hidden");
   hamburgerMenu.classList.toggle("transition-max-h");
+  hamburgerMenu.classList.remove("hamburgen-menu");
 });
